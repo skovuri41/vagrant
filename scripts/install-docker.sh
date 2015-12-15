cc811b7423a4d1979cc423b67f27df62d9002c43
shyam sunder k <surya46584@gmail.com>
2015-12-15 15:09:48 -0500

    install docker script

---
+++ scripts/install-docker.sh
@@ -0,0 +1,8 @@
+# install docker engine
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+rm -rf /etc/apt/sources.list.d/docker.list >> /dev/null
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get purge lxc-docker*
+apt-get install -y docker-engine
+
